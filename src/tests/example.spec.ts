0479d7ed055a591fab8975a4e2d1ac14bd03f70d
Elizabeth Chibvuri <Elizabeth@chilli.local>
2021-08-10 15:31:47 +0200

    tests

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,5 +1,41 @@
-describe('Thing', () => {
-    it('Should Pass', () => {
-        expect(true).toBe(true);
+describe('Game', () => {
+    describe('Play',()=>{
+        it('Given PlayerMove paper and OpponentMove rock should return player wins', () => {
+            //Arrange
+            let sut=new Game();
+            const playerMove = "Paper";
+            const Opponent = "Rock";
+            //Act
+            let actual=sut.Play(playerMove,Opponent);
+            //Assert
+            let expected="Player Wins";
+            expect(actual).toBe(expected);
+        })
+        it('Given PlayerMove paper and OpponentMove Scissors should return player loses', () => {
+            //Arrange
+            let sut=new Game();
+            const playerMove = "Paper";
+            const Opponent = "Scissors";
+            //Act
+            let actual=sut.Play(playerMove,Opponent);
+            //Assert
+            let expected="Player Loses";
+            expect(actual).toBe(expected);
+        })
     })
+  
 })
+
+export class Game{
+    Play(playerMove:string,opponentMove:string) {
+     
+        if(opponentMove==="Scissors")
+        {
+            return "Player Loses";
+        }
+
+        return "Player Wins";
+       
+    }
+
+}
